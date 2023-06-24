a3d9c0254813f8919adaac50cd6ce10987509fde
Lucas <garcia.lucas3110@icloudd.com>
2023-06-23 22:02:13 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// Responsive hamburger menu functionality
+const hamburger = document.querySelector('.hamburger');
+const responsiveMenu = document.createElement('div');
+responsiveMenu.classList.add('responsive-menu');
+
+// Create responsive menu
+const navItems = document.querySelectorAll('nav ul li');
+navItems.forEach(item => {
+    const link = item.querySelector('a');
+    const clone = link.cloneNode(true);
+    responsiveMenu.appendChild(clone);
+});
+
+// Toggle responsive menu on click
+hamburger.addEventListener('click', () => {
+    if (responsiveMenu.style.display === 'block') {
+        responsiveMenu.style.display = 'none';
+    } else {
+        responsiveMenu.style.display = 'block';
+    }
+});
+
+// Insert responsive menu into the DOM
+const header = document.querySelector('header');
+header.appendChild(responsiveMenu);
